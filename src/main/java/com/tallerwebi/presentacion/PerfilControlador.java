8adcb235689d137f00be105b820896ee6ef9da52
Ro Campagnoli <ro.campagnoli.18@gmail.com>
2026-05-17 22:41:02 -0300

    cambios de ivan

--- src/main/java/com/tallerwebi/presentacion/PerfilControlador.java
+++ src/main/java/com/tallerwebi/presentacion/PerfilControlador.java
@@ -1,33 +1,32 @@
 package com.tallerwebi.presentacion;
 
 import com.tallerwebi.dominio.Usuario;
+import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpSession;
-
 @Controller
 public class PerfilControlador {
 
-    @RequestMapping(path = "/perfil", method = RequestMethod.GET)
-    public ModelAndView irAlPerfil(HttpSession session) {
-        Usuario usuario = (Usuario) session.getAttribute("USUARIO");
-        if (usuario == null) {
-            return new ModelAndView("redirect:/login");
-        }
-        ModelAndView mv = new ModelAndView();
-        mv.addObject("usuario", usuario);
-        mv.setViewName("perfil");
-        return mv;
+  @RequestMapping(path = "/perfil", method = RequestMethod.GET)
+  public ModelAndView irAlPerfil(HttpSession session) {
+    Usuario usuario = (Usuario) session.getAttribute("USUARIO");
+    if (usuario == null) {
+      return new ModelAndView("redirect:/login");
     }
+    ModelAndView mv = new ModelAndView();
+    mv.addObject("usuario", usuario);
+    mv.setViewName("perfil");
+    return mv;
+  }
 
-    public ModelAndView mostrarDatosUsuario(HttpSession session) {
-        Usuario usuario = (Usuario) session.getAttribute("USUARIO");
-        ModelAndView mv = new ModelAndView();
-        mv.addObject("usuario", usuario);
-        mv.setViewName("perfil");
-        return mv;
-    }
+  public ModelAndView mostrarDatosUsuario(HttpSession session) {
+    Usuario usuario = (Usuario) session.getAttribute("USUARIO");
+    ModelAndView mv = new ModelAndView();
+    mv.addObject("usuario", usuario);
+    mv.setViewName("perfil");
+    return mv;
+  }
 }
