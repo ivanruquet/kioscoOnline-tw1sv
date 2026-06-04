1b92be69b52ba741f459f187f4f541ebcbe3746c
Ivan Ruquet <ivanruquet@gmail.com>
2026-06-04 17:14:02 -0300

    WIP: empiezo E2E de flujo a Mercado Pago, incompleto

---
+++ src/test/java/com/tallerwebi/punta_a_punta/FlujoHastaMercadoPagoE2E.java
@@ -0,0 +1,71 @@
+package com.tallerwebi.punta_a_punta;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserContext;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import com.tallerwebi.dominio.Productos.Producto;
+import com.tallerwebi.punta_a_punta.vistas.VistaHome;
+import com.tallerwebi.punta_a_punta.vistas.VistaLogin;
+import com.tallerwebi.punta_a_punta.vistas.VistaNuevoUsuario;
+import org.junit.jupiter.api.*;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.matchesPattern;
+import static org.hamcrest.text.IsEqualIgnoringCase.equalToIgnoringCase;
+
+public class FlujoHastaMercadoPagoE2E {
+
+    static Playwright playwright;
+    static Browser browser;
+    BrowserContext context;
+    VistaLogin vistaLogin;
+    VistaHome vistaHome;
+
+    @BeforeAll
+    static void abrirNavegador() {
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch();
+        //browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(500));
+    }
+
+    @AfterAll
+    static void cerrarNavegador() {
+        playwright.close();
+    }
+
+    @BeforeEach
+    void crearContextoYPagina() {
+        ReiniciarDB.limpiarBaseDeDatos();
+
+        context = browser.newContext();
+        Page page = context.newPage();
+        vistaLogin = new VistaLogin(page);
+    }
+
+    @AfterEach
+    void cerrarContexto() {
+        context.close();
+    }
+
+    //terminar
+    void deberiaRedirigirAMercadoPagoAlPagar()  {
+
+        String email = "test@unlam.edu.ar";
+        String clave = "test";
+
+        vistaLogin.escribirEMAIL(email);
+
+        vistaLogin.escribirClave(clave);
+
+        vistaLogin.darClickEnIniciarSesion();
+
+        vistaHome.agregarProductoAlCarrito();
+
+    }
+
+
+}
