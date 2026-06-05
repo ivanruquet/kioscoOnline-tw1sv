e0d2ada8ca9f9a4275fd59bed87ddf1a92f55843
Ivan Ruquet <ivanruquet@gmail.com>
2026-06-04 21:54:00 -0300

    fix: corregir errores de ESLint

--- src/main/webapp/resources/core/js/edicionPerfil.js
+++ src/main/webapp/resources/core/js/edicionPerfil.js
@@ -1,38 +1,38 @@
 /* eslint-disable no-unused-vars */
 function activarEdicion() {
-    document.getElementById("perfil-datos").classList.add("en-edicion");
+  document.getElementById("perfil-datos").classList.add("en-edicion");
 }
 
 function cancelarEdicion() {
-    document.getElementById("perfil-datos")
-        .classList.remove("en-edicion");
+  document.getElementById("perfil-datos")
+    .classList.remove("en-edicion");
 
-    const preview = document.getElementById("fotoPreview");
+  const preview = document.getElementById("fotoPreview");
 
-    preview.src = preview.dataset.original;
+  preview.src = preview.dataset.original;
 
-    document.getElementById("inputFoto").value = "";
+  document.getElementById("inputFoto").value = "";
 }
 
 function previsualizarFoto(url) {
-    if (url && url.startsWith("http")) {
-        document.getElementById("fotoPreview").src = url;
-    }
+  if (url && url.startsWith("http")) {
+    document.getElementById("fotoPreview").src = url;
+  }
 }
 
 document.getElementById("inputFoto")
-    .addEventListener("change", function (event) {
+  .addEventListener("change", function (event) {
 
-        const archivo = event.target.files[0];
+    const archivo = event.target.files[0];
 
-        if (archivo) {
-            const lector = new FileReader();
+    if (archivo) {
+      const lector = new FileReader();
 
-            lector.onload = function (e) {
-                document.getElementById("fotoPreview").src =
-                    e.target.result;
-            };
+      lector.onload = function (e) {
+        document.getElementById("fotoPreview").src =
+          e.target.result;
+      };
 
-            lector.readAsDataURL(archivo);
-        }
-    });
+      lector.readAsDataURL(archivo);
+    }
+  });
